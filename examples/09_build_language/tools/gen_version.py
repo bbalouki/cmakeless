90b6c0c31cb6058b6d7afc764e6856ae249f0d95
bbalouki <bbalouki@outlook.com>
2026-07-04 05:03:26 +0100

    Add license header to build_language gen_version.py

--- examples/09_build_language/tools/gen_version.py
+++ examples/09_build_language/tools/gen_version.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Writes generated/version.cpp: the code-generation step project.add_command() runs.
 
 Run by CMake through add_custom_command(), not directly; --out and --version
