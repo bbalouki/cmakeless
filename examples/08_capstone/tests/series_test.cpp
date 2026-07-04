f3fa3de59e14ebad4112682a041ab9721c320b39
bbalouki <bbalouki@outlook.com>
2026-07-04 02:59:50 +0100

    Add license header comment

--- examples/08_capstone/tests/series_test.cpp
+++ examples/08_capstone/tests/series_test.cpp
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 // GoogleTest suite for the stats library. cmakeless fetches GoogleTest (the
 // default framework), registers each case with CTest, and 'cmakeless test'
 // runs them all.
