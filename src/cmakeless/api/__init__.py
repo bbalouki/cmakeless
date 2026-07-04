f79ab8e1e7ac55f79d97c945ae53c9ffdb7df4a3
bbalouki <bbalouki@outlook.com>
2026-07-04 02:56:32 +0100

    Export When, Option, Command, CustomTarget from api package

--- src/cmakeless/api/__init__.py
+++ src/cmakeless/api/__init__.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Layer 1: what the user touches.
 
 Friendly, forgiving, mutable while the user is describing the build.
@@ -8,11 +12,14 @@ This package re-exports every public class and function from its submodules,
 so `from cmakeless.api import ...` reaches the whole layer-1 surface.
 """
 
+from cmakeless.api.commands import Command, CustomTarget
 from cmakeless.api.dependencies import Dependencies, Dependency
+from cmakeless.api.options import Option
 from cmakeless.api.presets import Preset
 from cmakeless.api.project import Project
 from cmakeless.api.targets import Executable, Library, PythonModule, Test
 from cmakeless.api.toolchains import Toolchain
+from cmakeless.api.when import When
 from cmakeless.observer import (
     BuildEvent,
     ConsoleObserver,
@@ -24,12 +31,15 @@ from cmakeless.observer import (
 
 __all__ = [
     "BuildEvent",
+    "Command",
     "ConsoleObserver",
+    "CustomTarget",
     "Dependencies",
     "Dependency",
     "Executable",
     "Library",
     "Observer",
+    "Option",
     "Preset",
     "Project",
     "PythonModule",
@@ -38,4 +48,5 @@ __all__ = [
     "StepStarted",
     "Test",
     "Toolchain",
+    "When",
 ]
