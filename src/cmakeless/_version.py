5018a75747e2adbb11a400c4624d7056f0c7bf53
bbalouki <bbalouki@outlook.com>
2026-07-04 02:56:32 +0100

    Bump version to 0.5.0

--- src/cmakeless/_version.py
+++ src/cmakeless/_version.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Single source of truth for the package version, read by hatchling at build time."""
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
