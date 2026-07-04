f13395dda9b04cbf1cc48c471a444f5f809b1d11
bbalouki <bbalouki@outlook.com>
2026-07-04 02:57:34 +0100

    Add license header comment

--- src/cmakeless/observer.py
+++ src/cmakeless/observer.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """The Observer event API: stable progress events for third-party consumers.
 
 Like the exception hierarchy in errors.py, this is cross-cutting: the driver
