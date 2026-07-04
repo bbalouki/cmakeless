b769822cabdd53dd67ed1b64da4f4b7926bd5385
bbalouki <bbalouki@outlook.com>
2026-07-04 02:57:07 +0100

    Add license header comment

--- src/cmakeless/driver/error_translation.py
+++ src/cmakeless/driver/error_translation.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Parse CMake and compiler output into structured diagnostics.
 
 The single biggest quality-of-life difference over raw CMake is how it feels
