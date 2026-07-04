bb38aca32ec236e54b2448123d59c7418d9a5205
bbalouki <bbalouki@outlook.com>
2026-07-04 02:58:50 +0100

    Add license header comment

--- tests/unittests/test_end_to_end_deps.py
+++ tests/unittests/test_end_to_end_deps.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """End-to-end dependency resolution against the real CMake engine.
 
 These tests download and compile a real third-party package, so they are
