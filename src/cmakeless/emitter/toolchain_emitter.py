ea52251dde7ae4422131b7202a7e67b868550ac4
bbalouki <bbalouki@outlook.com>
2026-07-04 02:57:25 +0100

    Add license header comment

--- src/cmakeless/emitter/toolchain_emitter.py
+++ src/cmakeless/emitter/toolchain_emitter.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Generated toolchain files for the simple cross-compilation cases.
 
 Wrapped toolchain files (Toolchain.from_file) are referenced in place and
