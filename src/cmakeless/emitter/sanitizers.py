59bf330b4aac052e6276eca936c9a984ccaf5ffa
bbalouki <bbalouki@outlook.com>
2026-07-04 02:57:25 +0100

    Add license header comment

--- src/cmakeless/emitter/sanitizers.py
+++ src/cmakeless/emitter/sanitizers.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Sanitizer names to per-compiler compile and link flag lines.
 
 Flags are always emitted in compile/link pairs behind compiler-id generator
