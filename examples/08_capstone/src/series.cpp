fab731ca27d034d032bf5ba590d8cfafc78ca1cd
bbalouki <bbalouki@outlook.com>
2026-07-04 02:59:50 +0100

    Add license header comment

--- examples/08_capstone/src/series.cpp
+++ examples/08_capstone/src/series.cpp
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 // The stats library implementation. fmt is a private dependency here: it is
 // used only for summary() and never appears in the public header.
 #include "stats/series.hpp"
