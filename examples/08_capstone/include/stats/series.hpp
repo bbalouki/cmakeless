4af1470ee61980984f3e067afd4c46162df03af0
bbalouki <bbalouki@outlook.com>
2026-07-04 02:59:49 +0100

    Add license header comment

--- examples/08_capstone/include/stats/series.hpp
+++ examples/08_capstone/include/stats/series.hpp
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 // The public face of the stats library: a Series of samples you can summarize.
 //
 // This header is what consumers include; the implementation (and its fmt
