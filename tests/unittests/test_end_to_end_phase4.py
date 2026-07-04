454d15e3244968df1f0ea0ca258bae46cb5d68d5
bbalouki <bbalouki@outlook.com>
2026-07-04 02:58:50 +0100

    Add license header comment

--- tests/unittests/test_end_to_end_phase4.py
+++ tests/unittests/test_end_to_end_phase4.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """End-to-end Phase 4 flows against the real CMake engine.
 
 targets_info() only needs CMake; the Python-module build additionally fetches
