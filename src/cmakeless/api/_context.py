1b92a873e4c1c76e5416660549337d76f55840c2
bbalouki <bbalouki@outlook.com>
2026-07-04 02:56:42 +0100

    Minor housekeeping

--- src/cmakeless/api/_context.py
+++ src/cmakeless/api/_context.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Private runtime context shared between the CLI and the API layer.
 
 Two pieces of state live here:
