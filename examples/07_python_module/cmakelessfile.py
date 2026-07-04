c098900a0800ed5e3c83daf8c682984d309928b2
bbalouki <bbalouki@outlook.com>
2026-07-04 02:59:41 +0100

    Add license header comment

--- examples/07_python_module/cmakelessfile.py
+++ examples/07_python_module/cmakelessfile.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Python and C++ interop: a real C++ extension you can import.
 
 add_python_module() fetches the binding backend (pybind11 by default), builds
